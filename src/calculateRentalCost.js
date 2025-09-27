bb502c956ed2fc546aabbed28e3973ae06a63c76
Maksym Marchenko <mm.forwork@gmail.com>
2025-09-27 15:14:14 +0300

    add fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days < 3) {
-    return days * 40;
-  } else if (days >= 3 && days < 7) {
-    return days * 40 - 20;
-  } else {
-    return days * 40 - 50;
+  const DAILY_RATE = 40;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const basePrice = days * DAILY_RATE;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
